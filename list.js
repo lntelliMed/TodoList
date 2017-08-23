ba5533af81e2506c6a47e9604e5ae68d3685cd13
Sam Alsmadi <Sam.Alsmadi@gmail.com>
2017-08-22 21:14:06 -0400

    Add todo item delete functionality

--- list.js
+++ list.js
@@ -3,11 +3,33 @@ var input = prompt("What would you like to do?");
 
 while (input !== "quit"){
 	if(input === "list"){
-		console.log(todos);
+		listTodos();
 	} else if (input === "new"){
-		var newTodo = prompt("Add a new todo item");
-		todos.push(newTodo);
-	}	
+		addTodo();
+	} else if (input === "delete"){
+		deleteTodo();
+	}
 	input = prompt("What would you like to do?");
 }
 console.log("Ok, you quit the app!");
+
+function listTodos(){
+	console.log("**********");
+	todos.forEach(function(todo, i){
+		console.log(i + ": " + todo);
+	});
+	console.log("**********");
+	//console.log(todos);
+}
+
+function addTodo(){
+	var newTodo = prompt("Add a new todo item");
+	todos.push(newTodo);
+	console.log("Added a new todo item");
+}
+
+function deleteTodo(){
+	var index = prompt("Enter the index of the todo item you want to delete");
+	todos.splice(index, 1);
+	console.log("The todo item at index " + index + " has been deleted!");
+}
